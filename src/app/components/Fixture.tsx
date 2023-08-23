3fd7fc3b73f111364570e606eaeeb71255467724
savagegraphics <olawaleraji188@gmail.com>
2023-08-23 04:32:01 -0700

    first commit

--- src/app/components/Fixture.tsx
+++ src/app/components/Fixture.tsx
@@ -8,7 +8,7 @@ const Fixture = (props: Props) => {
 	<div className="container lg:ml-32 ml-0 flex flex-col-reverse lg:flex-row">
 		<div className="flex flex-col px-6 py-8 space-y-6 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 dark:bg-violet-400 dark:text-gray-900">
 			<div className="flex space-x-2 sm:space-x-4">
-				<div className="space-y-2 mb-14">
+				<div className="space-y-2 lg:mb-14 mb-2">
 					<p className="flex items-center justify-center text-white text-4xl font-[2rem] mt-8 mb-14 text-left pr-8 leading-8 tracking-wide">About Us</p>
 					<p className="text-white lg:ml-[-3rem] ml-6 text-[1rem] font-[6px] text-left pr-8 leading-8 tracking-wide">At Construe, we take pride in building dreams from the ground up. 
           Established with a vision for excellence and passion for construction,
@@ -17,7 +17,7 @@ const Fixture = (props: Props) => {
             awe-inspiring structures.</p>
 				</div>
 			</div>
- <div className="mx-auto text-center mb-6 mt-12 ">
+ <div className="mx-auto text-center mb-1 mt-2">
  <h2 className="text-gray-100">Trusted by Open Source, enterprise, and more than 99,000 of you</h2>
 
   </div>
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -11,17 +11,17 @@ const Hero = (props: Props) => {
     <section className="p-6 bg-[#EE6401] text-gray-100 lg:h-[140vh]">
     <Navbar/>
 	<div className="container grid gap-6 mx-auto text-center lg:my-8 lg:grid-cols-2 xl:grid-cols-5">
-		<div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-2 dark:bg-gray-900">
-			<h1 className="flex items-start text-5xl lg:font-[24px] sm:font-[16px] dark:text-gray-50">We Build Very</h1>
-            <h1 className="flex items-start text-5xl lg:font-[24px] sm:font-[16px] dark:text-gray-50">Modern and</h1>
-        	<h1 className="flex items-start whitespace-nowrap text-5xl lg:font-[24px] sm:font-[16px] dark:text-gray-50">Stylish Buildings.</h1>
-		    <p className="flex items-start pt-8 font-medium dark:text-gray-50">Modular and versatile. Fugit vero facilis dolor sit</p>
-            <p className="flex items-start pb-8 font-medium dark:text-gray-50">neque cupiditate minus esse accusamus.</p>
+		<div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-2">
+			<h1 className="flex items-start lg:text-5xl text-2xl lg:font-[24px] sm:font-[16px]">We Build Very</h1>
+            <h1 className="flex items-start lg:text-5xl text-2xl lg:font-[24px] sm:font-[16px]">Modern and</h1>
+        	<h1 className="flex items-start whitespace-nowrap lg:text-5xl text-2xl lg:font-[24px] sm:font-[16px]">Stylish Buildings.</h1>
+		    <p className="flex items-start pt-8 font-medium">Modular and versatile. Fugit vero facilis dolor sit</p>
+            <p className="flex items-start pb-8 font-medium">neque cupiditate minus esse accusamus.</p>
           <button type="button" className="flex items-start px-8 py-3 bg-gray-100 text-gray-800 rounded-lg">Caret</button>
         </div>
-        <div className='relative xl:col-span-3 sm:mx-8'>
+        <div className='relative xl:col-span-3 sm:m-8 sm:mr-20'>
   <img src="https://images.unsplash.com/photo-1600047509807-ba8f99d2cdde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=692&q=80" 
-  alt="" className="object-cover lg:w-[737px] lg:h-[549px] sm:w-[487px] sm:h-[449px] rounded-md " />
+  alt="" className="object-cover lg:w-[737px] lg:h-[549px] sm:w-[447px] sm:h-[449px] rounded-md " />
 
   {/* Hide the second image on md and small screens */}
   <img
